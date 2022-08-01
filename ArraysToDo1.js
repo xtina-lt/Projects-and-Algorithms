9d293dd755f369ac193512e795aba646974b56a0
xtina-lt <xtina_lt@outlook.com>
2022-08-01 17:11:13 -0400

    comments

--- ArraysToDo1.js
+++ ArraysToDo1.js
@@ -1,62 +1,70 @@
 // swap first and last
 const example = () => {
     let arr = [1,2,3,4,5];
+    // initlaize temp at first
     let temp = arr[0]
+    // set first to last
     arr[0] = arr[arr.length-1];
+    // set last to temp
     arr[arr.length-1] = temp;
+
     console.log(arr)
 }
 
 const shiftleft = ()=> {
     let arr = [1,2,3,4,5]
+    // initialize a temp of first element
     let temp = arr[0]
-
+    // forward loop starting at arr[1]
     for (let i=1; i < arr.length; i++){
+        // LEFT ELEMENT = next element
         arr[i-1] = arr[i]
     }
-
+    // set last element to temp
     arr[arr.length-1] = temp
     console.log(arr)
 }
 
 const shiftRight = () => {
     let arr = [1,2,3,4,5]
+    // intitalize temp to last element
     let temp = arr[arr.length-1]
-
+    // backwards loop starts at end, goes to first, goes backwards
     for (let i=arr.length-1; i>0; i--){
+        // RIGHT ELEMENT = previous
         arr[i] = arr[i-1];
     }
-
+    // reintigrate temp
     arr[0] = temp;
     console.log(arr)
 }
 
-// ADD FIRST
+// ADD FIRST, shift right
 const push = (arr, num) => {
-    // 1) initialization
-    let last = arr[arr.length-1]
-
-    // 2) execution
-    // shift right
+    // intitalize temp to last element
+    let temp = arr[arr.length-1]
+    // backwards loop starts at end, goes to first, goes backwards
     for (let i=arr.length-1; i > 0; i--){
+        // RIGHT ELEMENT = previous
         arr[i] = arr[i-1]
     }
     // add first
     arr[0]=num
     // make last last
-    arr[arr.length]=last;
+    arr[arr.length]=temp;
 
     // 3) evaluation
     console.log(arr)
 }
 
-// REMOVE FIRST
+// REMOVE FIRST: shift left, shorten arr
 const pop = (arr) =>{
-    // 1) shift left
+    // forward loop starting at arr[1]
     for (let i=1; i<arr.length; i++){
+        // LEFT ELEMENT = next
         arr[i-1] = arr[i]
     }
-    // 2) decrease arr length
+    //decrease arr length
     arr.length = arr.length-1
 
     // evaluation
@@ -64,15 +72,17 @@ const pop = (arr) =>{
     return removed
 }
 
-const insertAt = (arr, index, num) => {
-    // 1) shift right
+// INSERT AT: shift right, insert number
+const insertAt = (arr, index, e) => {
+    // iterate backwards, from length, to index to be changed
     for (let i=arr.length; i >= index; i--){
+        // RIGHT element = previous
         arr[i] = arr[i-1];
     }
-    // 2) insert num
-    arr[index] = num
+    // change the element at index
+    arr[index] = e
 
-    // 2) evaluation - return/read
+    // evaluation
     console.log(arr)
 }
 
@@ -121,6 +131,7 @@ const removeAt = (arr, index) => {
             for (let j=i; j < arr.length-1; j++){
                 console.log(`J: arr[${j}]: ${arr[j]}`)
                 console.log(`J+1: arr[${j+1}]: ${arr[j+1]}`)
+                // LEFT ELEMENT = next
                 arr[j] = arr[j+1]
             }
         }
